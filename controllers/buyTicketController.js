699a048468e9991aee8f31ab5fe7de923d2296c8
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-09-12 21:14:25 +0300

    Added route that updates user and event after ticket purchase

--- controllers/buyTicketController.js
+++ controllers/buyTicketController.js
@@ -3,6 +3,51 @@ const client = require('../connection');
 
 const buyTicket = asyncHandler(async (req, res) => {
     console.log(req.body);
+    const { eventId, userId, ticketsAmount } = req.body;
+
+    //Finds the event and event data validations
+    const event = await client.query(`SELECT * FROM "upcomingEvents" WHERE id=${eventId}`);
+
+    if (event.rowCount !== 1) {
+        return res.status(400).json({ message: "No such event exists in the database" });
+    }
+
+    if (event.rows[0].places < ticketsAmount) {
+        return res.status(400).json({ message: "There aren't these amount of places for the events!" });
+    }
+
+    //Update user
+    const userQuery = `
+        UPDATE "users"
+        SET "moneySpent" = "moneySpent" + ${event.rows[0].price * ticketsAmount}, "willParticipate" = 
+                CASE WHEN array_positions("willParticipate", ${eventId}::text) IS NULL THEN array_append("willParticipate", ${eventId}::text)
+                ELSE "willParticipate"
+        END
+        WHERE id = ${userId}
+        RETURNING *
+    `;
+
+    const updatedUser = await client.query(userQuery);
+
+    if (updatedUser.rowCount === 0) {
+        return res.status(400).json({ message: "Error while updating your data. Please try again!" });
+    }
+
+    //Update event 
+    const eventQuery = `
+        UPDATE "upcomingEvents"
+        SET "places"="places"-${ticketsAmount}
+        WHERE id=${eventId}
+        RETURNING *
+    `;
+
+    const updatedEvent = await client.query(eventQuery);
+
+    if (updatedEvent.rowCount === 0) {
+        return res.status(400).json({ message: "Error while updating event data. Please try again!" });
+    }
+
+    return res.status(200).json({ message: "Purchase completed successfully!" });
 });
 
 module.exports = { buyTicket };
