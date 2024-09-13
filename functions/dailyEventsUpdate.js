646c889b555b2492a492b742ff675949f88568ba
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-09-13 18:55:19 +0300

    Added table for passed events and daily function for transfering events from upcomingEvents to passedEvents

---
+++ functions/dailyEventsUpdate.js
@@ -0,0 +1,75 @@
+const client = require('../connection');
+
+const dailyUpdate = async () => {
+    const currentDate = new Date();
+    const today = `${currentDate.getDay()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`;
+
+    //Gets every event from upcomingEvents table
+    const allUpcomingEvents = await client.query("SELECT * FROM \"upcomingEvents\"");
+
+    const passedEvents = [];
+
+    //Stores inside array every id of passed event
+    allUpcomingEvents.rows.forEach(event => {
+        if (event.event_date < currentDate) {
+            passedEvents.push(event.id);
+        }
+    });
+
+    //No events are passed for the current day
+    if (passedEvents.length === 0) {
+        console.log("No changed events on date - " + today);
+        return;
+    }
+
+    //Query for deleting all passed events
+    const deleteQuery = `
+        DELETE FROM "upcomingEvents"
+        WHERE id IN(${passedEvents.join(', ')})
+        RETURNING *
+    `;
+
+    const deletedEvents = await client.query(deleteQuery);
+
+    //No events were deleted
+    if (deletedEvents.rowCount === 0) {
+        console.log("No changed events on date - " + today);
+        return;
+    }
+
+    //Inserts every deleted event in the passed events database
+    const insertOperations = await Promise.all(deleteQuery.rows.map(async (event) => {
+        const insertQuery = `
+            INSERT INTO "passedEvents" (name, description, location, duration, price, "organizer_ID", image, event_date, places, participants)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        `;
+
+        const values = [
+            event.name,
+            event.description,
+            event.location,
+            event.duration,
+            event.price,
+            event.organizer_ID,
+            event.image,
+            event.event_date,
+            event.places,
+            event.participants
+        ];
+
+        const result = await client.query(insertQuery, values);
+
+        if (result.rowCount === 1) {
+            return "Success";
+        }
+        else {
+            return "Error while inserting";
+        }
+    }));
+
+    insertOperations.forEach(operation => console.log(operation));
+
+    console.log("Daily operation complete. Today dte - " + today);
+}
+
+module.exports = dailyUpdate;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const client = require('./connection.js');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
+const dailyUpdate = require('./functions/dailyEventsUpdate.js');
 const cors = require('cors');
 
 const PORT = 3000;
@@ -12,6 +14,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//Daily update of the events
+cron.schedule('0 0 * * *', async () => {
+    console.log('Running daily database update...');
+    await dailyUpdate();
+});
+
+//Called also on the start of the server in order to transfer events after the server has started
+dailyUpdate();
 
 app.use('/user', require('./routes/UserRoute'));
 app.use('/events', require('./routes/EventRoute'));
