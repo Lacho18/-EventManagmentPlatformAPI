ce5970b1fa450f4d25796a9f72c38d1e8892586a
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-09-20 21:58:20 +0300

    commit

--- routes/AllUsers.js
+++ routes/AllUsers.js
@@ -4,7 +4,17 @@ const client = require('../connection');
 
 router.route('/*')
     .get(async (req, res) => {
-        const allUsers = await client.query("SELECT * FROM users RETURNING *");
+        const { conditions } = JSON.parse(req.query.data);
+        const keys = Object.keys(conditions ? conditions : {});
+
+        let hasConditions = keys.length > 0 ? true : false;
+
+        let conditionsQuery = "";
+
+        if (hasConditions) {
+            conditionsQuery = keys.map(key => `"${key}" = '${conditions[key]}'`).join(',');
+        }
+        const allUsers = await client.query("SELECT * FROM users " + (hasConditions ? "WHERE " + conditionsQuery : ""));
 
         if (allUsers.rowCount > 0) {
             return res.status(200).json({ message: "All users data", allUsers: allUsers.rows });
