5b2500e2e4c0e0b6e8f121f0d8d3831c75e4f1ae
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-09-25 23:06:13 +0300

    commit

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -46,14 +46,42 @@ const getMessages = asyncHandler(async (req, res) => {
 const postMessage = asyncHandler(async (req, res) => {
     const data = req.body;
 
+    //Updating the chats array of the users
+    const updateUsersQuery = `
+        UPDATE "users"
+            SET "chats" = CASE
+            WHEN id = ${data.senderId} AND array_position("chats", ${data.receiverId}) IS NULL
+            THEN array_append("chats", ${data.receiverId})
+            WHEN id = ${data.receiverId} AND array_position("chats", ${data.senderId}) IS NULL
+            THEN array_append("chats", ${data.senderId})
+            ELSE "chats"
+        END
+        WHERE id IN (${data.senderId}, ${data.receiverId})
+        RETURNING *
+    `;
+
+    /*const updateUsers = await client.query(updateUsersQuery);
+
+    if (updateUsers.rowCount === 0) {
+        return res.status(400).json({ message: "Invalid message data send. Please try again!" });
+    }*/
+
+    //Post the message in the chats table
     const query = `
         INSERT INTO "chats" ("senderId", "receiverId", "message", "time_of_send")
         VALUES ($1, $2, $3, $4)
     `;
 
-    const values = [data.senderId, data.receiverId.data.message, new Date()];
+    const values = [data.senderId, data.receiverId, data.message, new Date()];
 
-    const result = await client.query(query, values);
+    /*const result = await client.query(query, values);
+
+    if (result.rowCount > 0) {
+        return res.status(200).json({ message: "Message posted!" });
+    }
+    else {
+        return res.status(400).json({ message: "Error while posting the message appeared." });
+    }*/
 });
 
 module.exports = { getMessages, postMessage };
